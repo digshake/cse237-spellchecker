b6a5a4341d67049a2ae832678bbdeb824f62bce0
Doug Shook <dsshook@gmail.com>
2020-03-04 11:09:55 -0600

    adding tests for real this time

--- src/tests/AddWordTests.java
+++ src/tests/AddWordTests.java
@@ -2,13 +2,38 @@ package tests;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class AddWordTests {
 
+	private SpellChecker sc;
+	
+	@BeforeEach
+	void setupTestingObjects() {
+		sc = new SpellChecker();
+	}
+	
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testAddNewWord() {
+		//run the behavior
+		sc.addWord("cat");
+		//check result
+		asserTrue(sc.checkWord("cat"));
+	}
+	
+	@Test
+	void testAddExistingWord() {
+		//run the behavior
+		sc.addWord("cat");
+		sc.addWord("cat");
+		//check result
+		assertTrue(true);//if i got here, no error happened
+	}
+	
+	@Test
+	void testAddWordDifferentCapitalization() {
+		
 	}
 
 }
